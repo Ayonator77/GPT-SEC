c1a52e9f4afc16eb13505bb5634f5ccdb26ebe6b
Ayonator77 <ayodejiodetola@gmail.com>
2024-02-28 12:25:11 -0500

    working on sorting data and getting openai api calls

--- GPT_SEC.py
+++ GPT_SEC.py
@@ -9,7 +9,7 @@ queryAPI = QueryApi(api_key='041c38e656c459b1b336a7bea7d83c057482f669d2e663a1b6f
 extractorApi = ExtractorApi(api_key='041c38e656c459b1b336a7bea7d83c057482f669d2e663a1b6fa8864b2071550')
 OPENEAI_API_KEY = "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"
 client = OpenAI(api_key=OPENEAI_API_KEY)
-
+categories_10k = ["1","1A", "1B", "2", "3", "4", "5", "6", "7", "7A", "8", "9", "9A", "9B", "10", "11", "12", "13", "14", "15"]
 
 #Class to strucutre EDGAR API query and get SEC filings
 class SEC_QUERY:
@@ -59,3 +59,58 @@ def EDGAR_CALL(query:SEC_QUERY, index):
 def get_section_text(query:SEC_QUERY, section:str, index):
     final_json, filing_url, q_dict = EDGAR_CALL(query, index)
     return extractorApi.get_section(filing_url, section, "text")
+
+def read_to_list(categ):
+    text_corpus = []
+    for cat in categ:
+        with open("SEC_TSLA_"+cat+".txt") as file:
+            text_corpus.append(file.read().replace('\n',' '))
+    
+    return text_corpus
+
+def token_chunk(text:str, max_token: int, prompt="Summarize the following text for me mention the company name and the specifics."):
+    encoding = tiktoken.get_encoding("cl100k_base")
+    token_int = encoding.encode(text) 
+
+    chunk_size = max_token - len(encoding.encode(prompt))
+
+    chunks = [token_int[i: i+chunk_size] for i in range(0, len(token_int), chunk_size)]
+
+    chunks = [encoding.decode(chunk) for chunk in chunks]
+    responses = []
+    message_construct = [{"role":"user", "content": "Summarize the following text for me mention the company name and the specifics."},
+                         {"role":"user", "content": "To provide the context for the above prompt, I will send you text in parts. When I am finished, I will tell you 'ALL PARTS SENT'. Do not answer until you have received all the parts. "}] #initial prompt
+
+    for chunk in chunks:
+        message_construct.append({"role":"user", "content":chunk})
+        while(sum(len(encoding.encode(msg["content"]))for msg in message_construct) > max_token):
+            message_construct.pop()
+        
+        response = client.chat.completions.create(
+            model = "gpt-3.5-turbo",
+            messages = message_construct
+        )
+        re_string = response.choices[0].message.content.strip()
+        responses.append(re_string)
+    
+    message_construct.append({"role": "user", "content": "ALL PARTS SENT"})
+
+    response = client.chat.completions.create(model = "gpt-3.5-turbo", messages = message_construct)
+    final_response = response.choices[0].message.content.strip()
+    responses.append(final_response)
+    return responses
+
+def split(text:str, prompt, max_token=4000):
+    encoding = tiktoken.get_encoding("cl100k_base")
+    token_int = encoding.encode(text)
+    chunk_size =  max_token - len(encoding.encode(prompt))
+    text_encoding = [token_int[i: i+chunk_size] for i in range(0, len(token_int), chunk_size)]
+    text_encoding = [encoding.decode(chunk) for chunk in text_encoding]
+    mid_point = (len(text_encoding) - 1) // 2
+    text_1 = text_encoding[0:mid_point]
+    text_2 = text_encoding[mid_point::]
+    return text_1, text_2
+
+if __name__ == "__main__":
+    text = read_to_list(categories_10k)
+    t1, t2 = split(text[0], prompt="Summarize the following text for me mention the company name and the specifics.")
